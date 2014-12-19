abdcd62b1f936f7f450a74b954673ad5b53508e8
Brian Oline <brian.oline@archoninfosys.com>
2014-12-19 16:59:21 -0600

    fixing bower path

--- require-config.js
+++ require-config.js
@@ -4,18 +4,18 @@ var require = (function (root) {
 		baseUrl: "./",
 		paths: {
 			"app": "./",
-			"jquery": "/bower_components/jquery/dist/jquery",
-			"lodash": "/bower_components/lodash/dist/lodash",
-			"knockout": "/bower_components/knockout.js/knockout",
+			"jquery": "./bower_components/jquery/dist/jquery",
+			"lodash": "./bower_components/lodash/dist/lodash",
+			"knockout": "./bower_components/knockout.js/knockout",
 			"bootstrap": "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min",
-			"knockout.punches": "/bower_components/knockout.punches/knockout.punches",
-			"template": "/bower_components/knockout-require-templates/template",
-			"text": "/bower_components/requirejs-text/text",
-			"stringTemplateEngine": "/bower_components/knockout-require-templates/stringTemplateEngine"
+			"knockout.punches": "./bower_components/knockout.punches/knockout.punches",
+			"template": "./bower_components/knockout-require-templates/template",
+			"text": "./bower_components/requirejs-text/text",
+			"stringTemplateEngine": "./bower_components/knockout-require-templates/stringTemplateEngine"
 		},
 		map: {
 			"*": {
-				'css': '../bower_components/require-css/css'
+				'css': './bower_components/require-css/css'
 			}
 		},
 		shim:{
