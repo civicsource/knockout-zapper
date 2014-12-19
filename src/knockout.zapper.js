624d224f6e86a540da5f7f167d107f9f7033f278
broline <brian.oline@gmail.com>
2014-12-18 20:50:32 -0600

    getting a working demo, need to fix test and clean up/polish.

--- src/knockout.zapper.js
+++ src/knockout.zapper.js
@@ -1,70 +1,85 @@
-(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["knockout", "jquery", "lodash", "./model",
-        "knockout.punches",
-        'template!./template/index.html!knockout-zapper-main'
-		], factory);
-	} else {
-		// Browser globals
-		factory(ko, $, _, ZapperModel);
-	}
-}(this, function (ko, $, _, ZapperModel) {
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(["knockout", "jquery", "lodash"], factory);
+    } else {
+        // Browser globals
+        factory(ko, $, _, ZapperModel);
+    }
+}(this, function(ko, $, _, ZapperModel) {
 
-	ko.bindingHandlers.zapper = {
-		init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-			var value = ko.utils.unwrapObservable(valueAccessor());
-			//element.model = new ZapperModel();
-			//element.model.instanceName = allBindingsAccessor.get('id') || _.uniqueId("zapper--");
-			//ko.renderTemplate("knockout-zapper-main", element.model, null, element, "replaceChildren");
+    ko.bindingHandlers.zapper = {
+        init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            //element.model = new ZapperModel();
+            //element.model.instanceName = allBindingsAccessor.get('id') || _.uniqueId("zapper--");
+            //ko.renderTemplate("knockout-zapper-main", element.model, null, element, "replaceChildren");
 
-			var $element = $(element),
-				$zappedTemplate,
-				$notZappedTemplate;
+            var $element = $(element),
+                $zappedTemplate,
+                $notZappedTemplate;
 
-			$notZappedTemplate = $element.find("div[data-not-zapped]").first();
-			$zappedTemplate = $element.find("div[data-zapped]").first();
+            $notZappedTemplate = $element.find("div[data-not-zapped]").first();
+            $zappedTemplate = $element.find("div[data-zapped]").first();
 
-			if (value === false) {
-				$notZappedTemplate.show();
-				$zappedTemplate.hide();
-			} else {
-				$notZappedTemplate.hide();
-				$zappedTemplate.show();
-			}
+            if (value === false) {
+                $notZappedTemplate.show();
+                $zappedTemplate.hide();
+            } else {
+                $notZappedTemplate.hide();
+                $zappedTemplate.show();
+            }
 
-		},
-		update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-			var value = ko.utils.unwrapObservable(valueAccessor());
+        },
+        update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var value = ko.utils.unwrapObservable(valueAccessor());
 
-			var settings = configureSettings(allBindingsAccessor.get('settings'));
+            var settings = configureSettings(allBindingsAccessor.get('settings'));
 
-			var $element = $(element),
-				$zappedTemplate,
-				$notZappedTemplate;
+            var $element = $(element),
+                $zappedTemplate,
+                $notZappedTemplate;
 
-			$notZappedTemplate = $element.find("div[data-not-zapped]").first();
-			$zappedTemplate = $element.find("div[data-zapped]").first();
+            $notZappedTemplate = $element.find("div[data-not-zapped]").first();
+            $zappedTemplate = $element.find("div[data-zapped]").first();
 
-			if (value === false) {
-				$notZappedTemplate.show();
-				$zappedTemplate.hide();
-			} else {
-				$notZappedTemplate.hide();
-				$zappedTemplate.show();
-			}
-		}
-	};
+            if (value === false) {
+                $notZappedTemplate.show();
+                $zappedTemplate.hide();
+            } else {
 
-	ko.virtualElements.allowedBindings.zapper = true;
+                //todo: check if this is a list etc.. if we are going to support other types of scenarios
+                $newList = $("<ul></ul>").attr('class', $element.parent().attr('class'));
+                var clone = $element.clone().appendTo($newList);
 
-	function configureSettings(config) {
-		if (!config) config = {};
-		var settings = ko.utils.unwrapObservable(config);
-		settings.zapEffect = valOrDefault(settings.zapEffect, "");
-	}
-	function valOrDefault(val, defValue) {
-		return val || defValue;
-	}
+                $newList.css({
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    left: $element.position().left,
+                    top: $element.position().top,
+                    zIndex: 1000000
+                }).appendTo($element.parent());
+                $zappedTemplate.show();
+                $newList.animate({
+                    left: $(window).width()
+                }, 1000, function() {
+                    $(this).remove();
+                });
+            }
+        }
+    };
+
+    ko.virtualElements.allowedBindings.zapper = true;
+
+    function configureSettings(config) {
+        if (!config) config = {};
+        var settings = ko.utils.unwrapObservable(config);
+        settings.zapEffect = valOrDefault(settings.zapEffect, "");
+    }
+
+    function valOrDefault(val, defValue) {
+        return val || defValue;
+    }
 
 }));
